e4f9892480b5545545148fe423084db91d10210e
quicks <133755446+manavk4214@users.noreply.github.com>
2025-01-17 16:06:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const loginBtn=document.getElementById('loginBtn')
+const register=document.getElementById("register")
+const Name=document.getElementById('name')
+const Email=document.getElementById('email')
+const mobile=document.getElementById('mobile')
+loginBtn.onclick=function(){
+    window.location.href="register.html"
+}
